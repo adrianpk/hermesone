575058b0aef676307b57e549ed24a3a3745ff679
Adrian PK <adrianpk@adrianpk.com>
2024-11-11 17:22:12 +0100

    Improve publish command

--- internal/hermes/git.go
+++ internal/hermes/git.go
@@ -3,15 +3,32 @@ package hermes
 import (
 	"bytes"
 	"fmt"
+	"log"
 	"os"
 	"os/exec"
+	"strings"
 )
 
 const (
-	outputDir = "output"
-	repoURL   = "https://github.com/adrianpk/sample-site.git"
-	branch    = "gh-pages"
+	outputDir   = "output"
+	// Following values are hardocoded for now but they will be configurable soon.
+	repoURL     = "https://github.com/adrianpk/adrianpk.github.io"
+	branch      = "gh-pages"
+	sourceBranch = "main"
 )
+
+func runCommand(name string, args ...string) (string, string, error) {
+	cmd := exec.Command(name, args...)
+	var out bytes.Buffer
+	var stderr bytes.Buffer
+	cmd.Stdout = &out
+	cmd.Stderr = &stderr
+	err := cmd.Run()
+	if err != nil {
+		return out.String(), stderr.String(), fmt.Errorf("%s: %s", err, stderr.String())
+	}
+	return out.String(), stderr.String(), nil
+}
 
 func PublishToGitHubPages() error {
 	err := os.Chdir(outputDir)
@@ -21,27 +38,42 @@ func PublishToGitHubPages() error {
 
 	_, err = os.Stat(".git")
 	if os.IsNotExist(err) {
-		_, err = runCommand("git", "init")
+		_, _, err = runCommand("git", "init")
 		if err != nil {
 			return err
 		}
-		_, err = runCommand("git", "remote", "add", "origin", repoURL)
+		_, _, err = runCommand("git", "remote", "add", "origin", repoURL)
 		if err != nil {
 			return err
+		}
+	} else {
+		stdout, stderr, err := runCommand("git", "status", "--porcelain")
+		if err != nil {
+			return fmt.Errorf("git status error: %s: stdout: %s, stderr: %s", err, stdout, stderr)
+		}
+		if stdout != "" {
+			log.Println("Uncommitted changes detected, committing them.")
 		}
 	}
 
-	_, err = runCommand("git", "add", ".")
+	_, _, err = runCommand("git", "add", ".")
 	if err != nil {
 		return err
 	}
 
-	out, err := runCommand("git", "commit", "-m", "Deploy to GitHub Pages")
-	if err != nil {
-		return fmt.Errorf("git commit error: %s: %s", err, out)
+	stdout, stderr, err := runCommand("git", "commit", "-m", "Deploy to GitHub Pages")
+	if err != nil && !strings.Contains(stderr, "nothing to commit") {
+		return fmt.Errorf("git commit error: %s: stdout: %s, stderr: %s", err, stdout, stderr)
 	}
 
-	_, err = runCommand("git", "push", "-f", "origin", "main:"+branch)
+	stdout, stderr, err = runCommand("git", "pull", "origin", branch, "--rebase")
+	if err != nil && strings.Contains(stderr, fmt.Sprintf("couldn't find remote ref %s", branch)) {
+		log.Printf("Remote branch %s does not exist, skipping pull step.", branch)
+	} else if err != nil {
+		return fmt.Errorf("git pull error: %s: stdout: %s, stderr: %s", err, stdout, stderr)
+	}
+
+	_, _, err = runCommand("git", "push", "origin", sourceBranch+":"+branch)
 	if err != nil {
 		return err
 	}
@@ -52,17 +84,4 @@ func PublishToGitHubPages() error {
 	}
 
 	return nil
-}
-
-func runCommand(name string, args ...string) (string, error) {
-	cmd := exec.Command(name, args...)
-	var out bytes.Buffer
-	var stderr bytes.Buffer
-	cmd.Stdout = &out
-	cmd.Stderr = &stderr
-	err := cmd.Run()
-	if err != nil {
-		return stderr.String(), fmt.Errorf("%s: %s", err, stderr.String())
-	}
-	return out.String(), nil
 }
